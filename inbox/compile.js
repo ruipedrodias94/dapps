ab8f54d4c2cdd69d8b24d85f92fbf38944eb227e
Rui Pedro Dias <ruypedrodias94@gmail.com>
2019-06-25 20:55:53 +0100

    Updated compile.js
    Updated the compile.js file.
    
    Now it passes the ABI, and Bytecode of the contract to deploy :+1:
    
    The test was also modified. Now I had to add some options to the web3 provider. :angry:

--- inbox/compile.js
+++ inbox/compile.js
@@ -5,114 +5,25 @@ const path = require('path');
 const solc = require('solc');
 const fs = require('fs-extra');
 
-// Functions
+const contractPath = path.resolve(__dirname, 'contracts','inbox.sol');
+const source = fs.readFileSync(contractPath, 'utf8');
 
-/**
- * Makes sure that the build folder is deleted, before every compilation
- * @returns {*} - Path where the compiled sources should be saved.
- */
-function compilingPreperations() {
-    const buildPath = path.resolve(__dirname, 'build');
-    fs.removeSync(buildPath);
-    return buildPath;
-}
-
-/**
- * Returns and Object describing what to compile and what need to be returned.
- */
-function createConfiguration() {
-    return {
-        language: 'Solidity',
-        sources: {
-            'inbox.sol': {
-                content: fs.readFileSync(path.resolve(__dirname, 'contracts', 'inbox.sol'), 'utf8')
-            },/*
-            'AnotherFileWithAnContractToCompile.sol': {
-                content: fs.readFileSync(path.resolve(__dirname, 'contracts', 'AnotherFileWithAnContractToCompile.sol'), 'utf8')
-            }*/
+let jsonContractSource = JSON.stringify({
+    language: 'Solidity',
+    sources: {
+      'Inbox': {
+          content: source,
+       },
+    },
+    settings: { 
+        outputSelection: {
+            '*': {
+                '*': ['abi',"evm.bytecode"],   
+             // here point out the output of the compiled result
+            },
         },
-        settings: {
-            outputSelection: { // return everything
-                '*': {
-                    '*': ['*']
-                }
-            }
-        }
-    };
-}
+    },
+});
 
-/**
- * Compiles the sources, defined in the config object with solc-js.
- * @param config - Configuration object.
- * @returns {any} - Object with compiled sources and errors object.
- */
-function compileSources(config) {
-    try {
-        return JSON.parse(solc.compile(JSON.stringify(config), getImports));
-    } catch (e) {
-        console.log(e);
-    }
-}
 
-/**
- * Searches for dependencies in the Solidity files (import statements). All import Solidity files
- * need to be declared here.
- * @param dependency
- * @returns {*}
- */
-
-function getImports(dependency) {
-    console.log('Searching for dependency: ', dependency);
-    switch (dependency) {
-        case 'inbox.sol':
-            return {contents: fs.readFileSync(path.resolve(__dirname, 'contracts', 'inbox.sol'), 'utf8')};
-        /*case 'AnotherImportedSolidityFile.sol':
-            return {contents: fs.readFileSync(path.resolve(__dirname, 'contracts', 'AnotherImportedSolidityFile.sol'), 'utf8')};*/
-        default:
-            return {error: 'File not found'}
-    }
-}
-
-/**
- * Shows when there were errors during compilation.
- * @param compiledSources
- */
-function errorHandling(compiledSources) {
-    if (!compiledSources) {
-        console.error('>>>>>>>>>>>>>>>>>>>>>>>> ERRORS <<<<<<<<<<<<<<<<<<<<<<<<\n', 'NO OUTPUT');
-    } else if (compiledSources.errors) { // something went wrong.
-        console.error('>>>>>>>>>>>>>>>>>>>>>>>> ERRORS <<<<<<<<<<<<<<<<<<<<<<<<\n');
-        compiledSources.errors.map(error => console.log(error.formattedMessage));
-    }
-}
-
-/**
- * Writes the contracts from the compiled sources into JSON files, which you will later be able to
- * use in combination with web3.
- * @param compiled - Object containing the compiled contracts.
- * @param buildPath - Path of the build folder.
- */
-function writeOutput(compiled, buildPath) {
-    let fileNameTemp;
-    fs.ensureDirSync(buildPath);
-    for (let contractFileName in compiled.contracts) {
-        console.log(contractFileName);
-        let contractName = contractFileName.replace(/.sol/gm, '');
-        let str = contractName.replace(/\s/g, "");
-        fs.outputJsonSync(
-            path.resolve(buildPath, str + '.json'),
-            compiled.contracts[contractFileName]
-       );
-       fileNameTemp = contractFileName;
-    }
-    return fileNameTemp;
-}
-
-// Workflow
-
-const buildPath = compilingPreperations();
-const config = createConfiguration();
-const compiled = compileSources(config);
-errorHandling(compiled);
-const fileName = writeOutput(compiled, buildPath);
-module.exports = compiled.contracts[fileName]; 
+module.exports = JSON.parse(solc.compile(jsonContractSource)).contracts.Inbox.Inbox;
--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -1,19 +1,33 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const OPTIONS = {
+    defaultBlock: "latest",
+    transactionConfirmationBlocks: 1,
+    transactionBlockTimeout: 5
+  };
+//ganache.provider(), null, OPTIONS
+let web3 = new Web3(ganache.provider(), null, OPTIONS);
+
+const {abi, evm} = require('../compile');
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
 
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
+
     //User one of those accounts to deploy the smartcontracts
+    inbox = await new web3.eth.Contract((abi))
+    .deploy({ data : '0x' + evm.bytecode.object, arguments: ['Hi there!']})
+    .send({ from : accounts[0], gas : '1000000'})
 });
 
 describe('Inbox starting', () => {
     it('Deploys a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
+       // console.log(bytecode);
     });
 });
